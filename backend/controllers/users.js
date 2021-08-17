6e70d07d776317e53d4d7868604225a6d5786d68
sanjanastevska <sanjanastevska@gmail.com>
2021-08-17 22:34:31 +0200

    Create and testing Endpoints for users using Postman

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
 const { generateToken } = require('../utils');
 
 const fetch = async (_, res, next) => {
@@ -19,13 +20,6 @@ const fetch = async (_, res, next) => {
 
 const register = async (req, res, next) => {
     try {
-        if (!req.body.password || req.body.password !== req.body.confirmation_password) {
-            return res.status(400).send({
-                error: true,
-                message: 'Bad request. Passwords do not match.'
-            });
-        }
-
         const user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).send({
@@ -55,27 +49,71 @@ const register = async (req, res, next) => {
 
 const login = async (req, res, next) => {
     try {
+        const user = await User.findOne({ email: req.body.email });
 
+        if(!user) {
+            return res.status(400).send({
+                error: true,
+                message: 'No user with the provided email'
+            })
+        }
+
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.status(400).send({
+                error: true,
+                message: 'Incorrect password'
+              });
+        }
+
+        res.status(201).send({
+            error: false,
+            message: 'You are logged in. JWT successfully generated',
+            token: generateToken(user),
+            user
+        });
     } catch (err) {
-
+        res.status(500).send({
+            error: true,
+            message: err.message
+        });
     }
     await next;
 };
 
 const update = async (req, res, next) => {
     try {
+        const user = await User.findByIdAndUpdate({ _id: req.params.id}, req.body);
 
+        if (req.body.password) {
+            user.password = bcrypt.hashSync(req.body.password, 8);
+        }
+        
+        res.status(200).send({
+            error: false,
+            message: 'User is updated!',
+            user
+        });
     } catch (err) {
-
+        res.status(500).send({
+            error: true,
+            message:err.message
+        });
     }
     await next;
 }
 
 const del = async (req, res, next) => {
     try {
-
-    } catch (err) {
-
+        const user = await User.deleteOne({ _id: req.params.id});
+        res.status(200).send({
+            error: false,
+            message: 'User Deleted!',
+            user
+        });
+    } catch(err) {
+        res.status(404).send({ 
+            message: 'User Not Found' 
+        });
     }
     await next;
 }
