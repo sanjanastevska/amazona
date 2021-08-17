99f411fa10e044c7b73e8637f2e589452a600be0
sanjanastevska <sanjanastevska@gmail.com>
2021-08-17 22:35:22 +0200

    create and testing endpoints for users using Postman

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -19,18 +19,18 @@ const userSchema = mongoose.Schema({
         default: false, 
         required: true 
     },
-    isSeller: { 
-        type: Boolean, 
-        default: false, 
-        required: true 
-    },
-    seller: {
-      name: String,
-      logo: String,
-      description: String,
-      rating: { type: Number, default: 0, required: true },
-      numReviews: { type: Number, default: 0, required: true },
-    }
+    // isSeller: { 
+    //     type: Boolean, 
+    //     default: false, 
+    //     required: true 
+    // },
+    // seller: {
+    //   name: String,
+    //   logo: String,
+    //   description: String,
+    //   rating: { type: Number, default: 0, required: true },
+    //   numReviews: { type: Number, default: 0, required: true },
+    // }
 }, {
     timestamps: true
 });
