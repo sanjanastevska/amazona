4413cb4f9f2768d85736148741060012070c358d
sanjanastevska <sanjanastevska@gmail.com>
2021-09-03 16:17:23 +0200

    Build Product Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import data from '../data';
+import Rating from '../components/Rating';
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    const product = data.products.find(x => x._id === props.match.params.id); 
+    if(!product) {
+        return <div>Product Not Found</div>
+    }
     return (
         <div>
-            <div className="row">
+            <Link to="/">Back to result</Link>
+            <div className="row top">
                 <div className="col-2">
-                    
+                    <img className="large" src={product.image} alt={product.name}></img>
+                </div>
+                <div className="col-1">
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            <Rating
+                                rating={product.rating}
+                                numReviews={product.numReviews}
+                            />
+                        </li>
+                        <li>${product.price}</li>
+                        <li>
+                            Description:
+                            <p>{product.description}</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-1">
+                    <div className="card card-body">
+                        <ul>
+                            <li>
+                                <div className="row">
+                                    <div>Price</div>
+                                    <div className="price">${product.price}</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="row">
+                                    <div>Status</div>
+                                    <div>
+                                        {product.countInStock > 0 ? <span className="success">In Stock</span> :
+                                        <span className="danger">Unavailable</span>}
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <button className="primary block">Add To Cart</button>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
